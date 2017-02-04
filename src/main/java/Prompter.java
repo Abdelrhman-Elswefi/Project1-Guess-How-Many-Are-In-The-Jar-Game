d68126ba574a9db77fd706568e1098f43aa7e9e9
Youssef Girgeis <youssefgirgeis@gmail.com>
2017-02-04 14:36:28 -0500

    Update Prompter.java

--- src/main/java/Prompter.java
+++ src/main/java/Prompter.java
@@ -8,13 +8,14 @@ public class Prompter {
 	public void promptForAdminstrator(){
 		
 
-		System.out.println("ADMINISTRATOR SETUP\n====================");
-		System.out.print("Name of items in the Jar: ");
+		System.out.println("ADMINISTRATOR SETUP\n------------------------");
+		System.out.print("What type of item should go in the jar: ");
 
 		String itemName = scanner.nextLine();
 
-		System.out.printf("Maximum number of %s in the jar: ", itemName);
+		System.out.printf("what is the maximum amount of %s that go in the jar: ", itemName);
 		int maxNumber = scanner.nextInt();
+		System.out.println("--------------------------\n\n\n\n\n");
 
 		jar = new Jar(itemName, maxNumber);
 
@@ -22,11 +23,9 @@ public class Prompter {
 	
 	public void promptForPlayer(){
 		
-		System.out.println("PLAYER\n=================");
-		System.out.printf("Your goal is to guess how many %s are in the jar. Your guess should be between 1 and %d.%n",
-				jar.getItemName(), jar.getMaxNumberItems());
-		
+		System.out.println("------------------------------\nWelcome to the guessing game!");
 		jar.guessNumber();
+		
 	}
 
 }
